f8a1837d58aaed5345a8da6c2a2107ebeb2504a1
Philipp Seitz <philipp.seitz@tum.de>
2020-05-12 16:43:50 +0200

    lint network code

--- quantum/network.py
+++ quantum/network.py
@@ -1,22 +1,34 @@
+"""This module provides the neural network part of the implementation"""
 import numpy as np
 
-from data_utils import generate_dataset
-from utils import run_circuit
+from quantum.data_utils import generate_dataset
+from quantum.utils import run_circuit
 
 DIMENSION = 4
 
 
 def init_weights(dimension):
-    weights = []
+    """Generates dimension-1 weights for the neural network.
+
+    Args:
+        dimension: Amount of weights do generate.
+
+    Returns:
+        weights_: List of np.arrays with shape(dimension-1, 16).
+    """
+    weights_ = []
     for _ in range(dimension - 1):
         weight = np.random.random(4**2)  #TODO check init of wheights
-        weights.append(weight)
-    return weights
+        weights_.append(weight)
+    return weights_
 
 
 if __name__ == "__main__":
-    (x_train, y_train), (x_test, y_test) = generate_dataset(DIMENSION)
-    image = x_train[0].flatten()
-    weights = init_weights(DIMENSION**2)
-    prediction = run_circuit(image, weights)
+    (X_TRAIN, Y_TRAIN), (X_TEST, Y_TEST) = generate_dataset(DIMENSION)
+    WEIGHTS = init_weights(DIMENSION**2)
+    for index, image in enumerate(X_TRAIN):
+        image = image.flatten()
+        if index == 0:
+            prediction = run_circuit(image, WEIGHTS)
+        #TODO update step
     print(f' Prediction: {prediction}')
