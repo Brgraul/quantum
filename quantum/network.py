9bc5cec905ca75ec6739ff186a1f23eea98b38be
Philipp Seitz <philipp.seitz@tum.de>
2020-05-19 15:52:04 +0200

    add batch loss and epoch training

--- quantum/network.py
+++ quantum/network.py
@@ -1,7 +1,7 @@
 """This module provides the neural network part of the implementation."""
 import numpy as np
 
-from quantum.data_utils import generate_dataset
+from quantum.data_utils import generate_dataset, iterate_minibatches
 from quantum.quantum_utils import run_circuit
 
 DIMENSION = 4
@@ -24,13 +24,30 @@ def init_weights(dimension):
     return weights_
 
 
-def loss(prediction, label, lambda_= 0.234, eta = 5.59):
+def loss(prediction, label, lambda_=0.234, eta=5.59):
+    #TODO add train accuracy
     runs = 1024
-    p_label = prediction.pop(LABELS[label], None) / runs 
+    p_label = prediction.pop(LABELS[label], None) / runs
     p_max_not = max(prediction.values()) / runs
-    return max(p_max_not-p_label + lambda_, 0) ** eta
-    
-def ()
+    return max(p_max_not - p_label + lambda_, 0)**eta
+
+
+def batch_loss(batch):
+    loss_ = 0
+    x_batch, y_batch = batch
+    for image, label in zip(x_batch, y_batch):
+        prediction = run_circuit(image.flatten(), WEIGHTS)
+        loss_ += loss(prediction, label)
+    return loss_ / len(batch[0])
+
+
+def train_epochs(x_train, y_train, batchsize=222, epochs=30):
+
+    for epoch in range(epochs):
+        for batch in iterate_minibatches(x_train, y_train, batchsize):
+            b_loss = batch_loss(batch)
+            #TODO update step
+
 
 if __name__ == "__main__":
     (X_TRAIN, Y_TRAIN), (X_TEST, Y_TEST) = generate_dataset(DIMENSION,
@@ -38,12 +55,4 @@ if __name__ == "__main__":
                                                             value_true=4,
                                                             value_false=9)
     WEIGHTS = init_weights(DIMENSION**2)
-    index = 0
-    for image, label in zip(X_TRAIN, Y_TRAIN):
-        image = image.flatten()
-        if index == 0:
-            index = 1
-            prediction = run_circuit(image, WEIGHTS)
-            loss(prediction, label)
-        #TODO update step
-    print(f' Prediction: {prediction}')
+    train_epochs(X_TRAIN, Y_TRAIN)
